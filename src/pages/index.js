9977a917e1cb5986b241c23f3ff90d0b8cd267df
Torie Joy-Warren <victoria.joywarren@gmail.com>
2021-04-18 18:30:37 -0400

    Update features section copy

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,8 +28,8 @@ function IndexPage(props) {
         size="md"
         bgImage=""
         bgImageOpacity={1}
-        title="Features"
-        subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud."
+        title="How it works"
+        subtitle="Sign up to receive text notifications when vaccine appointments become available in your area in a few simple steps."
       />
       <FaqSection
         bg="white"
