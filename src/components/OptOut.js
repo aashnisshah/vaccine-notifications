58f80ada12f61be73dd63a2ca40e1eaf93abd3c1
Torie Joy-Warren <torie@carezone.com>
2021-04-18 08:48:13 -0400

    Update user's optout status

--- src/components/OptOut.js
+++ src/components/OptOut.js
@@ -4,11 +4,14 @@ import FormAlert from "./FormAlert";
 import Button from "react-bootstrap/Button";
 import Spinner from "react-bootstrap/Spinner";
 import analytics from "./../util/analytics.js";
+import { updateUser } from "./../util/db";
+import { useAuth } from './../util/auth.js';
 
 function OptOut(props) {
   const [pending, setPending] = useState(false);
-
   const [formAlert, setFormAlert] = useState(null);
+
+  const auth = useAuth();
 
   const RECEIVED_DOSE_OPT_OUT_REASON = "received_dose";
   const OTHER_OPT_OUT_REASON = "other";
@@ -26,24 +29,51 @@ function OptOut(props) {
 
     let optOutReason = event.target[0].value;
     if (!OPT_OUT_REASONS.includes(optOutReason)) {
-      setFormAlert({
-        type: "error",
-        message: "Please select a reason for opting out",
-      });
+      setFormErrorMessage("Please select a reason for opting out");
       setPending(false);
       return;
     }
 
-    // Opt user out of notifications
-    // TODO
+    if (auth.user.optout) {
+      setOptOutSuccessMessage("You have already opted out of notifications.")
+      setPending(false);
+      return;
+    }
 
-    // Track opt out reason
-    analytics.track('optOut', {
-      reason: optOutReason
+    optOutUser()
+      .then(() => {
+        trackOptOutReason(optOutReason);
+        setOptOutSuccessMessage("You have successfully opted out of notifications.");
+      })
+      .catch((error) => {
+        setFormErrorMessage("There was an error updating your preferences. Please try again.");
+      })
+      .finally(() => {
+        setPending(false);
+      });
+  }
+
+  const optOutUser = async () => {
+    let data = { optout: true };
+    await updateUser(auth.user.uid, data);
+  }
+
+  const setFormErrorMessage = (message) => {
+    setFormAlert({
+      type: "error",
+      message: message,
     });
+  }
 
-    // Hide pending indicator
-    setPending(false);
+  const setOptOutSuccessMessage = (message) => {
+    setFormAlert({
+      type: "success",
+      message: message,
+    });
+  }
+
+  const trackOptOutReason = (optOutReason) => {
+    analytics.track('optOut', { reason: optOutReason });
   }
 
   return (
