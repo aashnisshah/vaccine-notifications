f019d7aec73deb013e269b36d8f617ffbfa2cb4c
Aashni <contact@aashni.me>
2021-04-21 03:04:06 -0400

    Fixed sending messages bug

--- api/_db.js
+++ api/_db.js
@@ -5,94 +5,108 @@ const firestore = firebaseAdmin.firestore();
 
 // Get user by uid
 function getUserByPhoneNumber(phone) {
-  return firestore.collection("users").where("phone", "==", phone).get().then(format);
+    return firestore
+        .collection("users")
+        .where("phone", "==", phone)
+        .get()
+        .then(format);
 }
 
 function getAllUsers() {
-  return firestore.collection("users").get().then(format);
+    return firestore.collection("users").get().then(format);
 }
 
 function getAllVerifiedUsers() {
-  return firestore.collection("users").where("verified", "==", "true").get().then(format);
+    return firestore
+        .collection("users")
+        .where("verified", "==", "true")
+        .get()
+        .then(format);
 }
 
-async function getTargettedUsers(province, postalCodes, ageGroups, eligibilityGroups) {
-  console.log(`query: province: ${province},
-    postalCodes: ${postalCodes},
-    ageGroups: ${ageGroups},
-    eligibilityGroups: ${eligibilityGroups}
-  `);
-  
-  let query = firestore.collection("users").where("optout", "==", false);
+async function getTargettedUsers(
+    province,
+    postalCodes,
+    ageGroups,
+    eligibilityGroups
+) {
+    let query = firestore.collection("users").where("optout", "==", false);
 
-  if (province && province != "CA") {
-    query = query.where("province", "==", province);
-  } else if (postalCodes.length > 0) {
-    query = query.where("postalShort", "in", postalCodes);
-  }
+    if (postalCodes && postalCodes.length > 0) {
+        query = query.where("postalShort", "in", postalCodes);
+    } else if (province && province != "CA") {
+        query = query.where("province", "==", province);
+    }
 
-  let targettedUsers = [];
+    let targettedUsers = [];
 
-  let resp = await query.get().then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-      let userData = doc.data();
-      let isTarget = true;
+    await query.get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            let userData = doc.data();
+            let isTarget = true;
 
-      // check if overlapping ageGroups
-      if (ageGroups &&
-        ageGroups.length > 0 &&
-        userData.ageGroups &&
-        userData.ageGroups.length > 0
-      ) {
-        if (_.intersection(userData.ageGroups, ageGroups).length == 0) {
-          isTarget = false;
-        }
-      }
+            // check if overlapping ageGroups
+            if (
+                ageGroups &&
+                ageGroups.length > 0 &&
+                userData.ageGroups &&
+                userData.ageGroups.length > 0
+            ) {
+                if (
+                    _.intersection(userData.ageGroups, ageGroups).length === 0
+                ) {
+                    isTarget = false;
+                }
+            }
 
-      // check if overlapping eligibilityGroups
-      if (eligibilityGroups &&
-        eligibilityGroups.length > 0 &&
-        userData.eligibilityGroups &&
-        userData.eligibilityGroups.length > 0
-      ) {
-        if (_.intersection(userData.eligibilityGroups, eligibilityGroups).length == 0) {
-          isTarget = false;
-        }
-      }
+            // check if overlapping eligibilityGroups
+            if (
+                eligibilityGroups &&
+                eligibilityGroups.length > 0 &&
+                userData.eligibilityGroups &&
+                userData.eligibilityGroups.length > 0
+            ) {
+                if (
+                    _.intersection(
+                        userData.eligibilityGroups,
+                        eligibilityGroups
+                    ).length === 0
+                ) {
+                    isTarget = false;
+                }
+            }
 
-      if (isTarget && userData.phoneNumber) {
-        targettedUsers.push(
-          {id: userData.uid,
-          phoneNumber: userData.phoneNumber
+            if (isTarget && userData.phoneNumber) {
+                targettedUsers.push({
+                    id: userData.uid,
+                    phoneNumber: userData.phoneNumber,
+                });
+            }
         });
-      }
     });
-  });
 
-  return targettedUsers;
+    return targettedUsers;
 }
-
-
 
 /**** HELPERS ****/
 
 // Format Firestore response (handles a collection or single doc)
 function format(response) {
-  if (response.docs) {
-    return response.docs.map(getDoc);
-  } else {
-    return getDoc(response);
-  }
+    if (response.docs) {
+        return response.docs.map(getDoc);
+    } else {
+        return getDoc(response);
+    }
 }
 
 // Get doc data and merge in doc.id
 function getDoc(doc) {
-  return doc.exists ? { id: doc.id, ...doc.data() } : null;
+    return doc.exists ? { id: doc.id, ...doc.data() } : null;
 }
 
 module.exports = {
-  getUserByPhoneNumber,
-  getAllUsers,
-  getAllVerifiedUsers,
-  getTargettedUsers
+    getUserByPhoneNumber,
+    getAllUsers,
+    getAllVerifiedUsers,
+    getTargettedUsers,
 };
--- api/twilio-send-bulk-message.js
+++ api/twilio-send-bulk-message.js
@@ -1,131 +1,141 @@
-const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+const client = require("twilio")(
+    process.env.TWILIO_ACCOUNT_SID,
+    process.env.TWILIO_AUTH_TOKEN
+);
 const _ = require("lodash");
-const {
-  getAllVerifiedUsers,
-  getTargettedUsers
-} = require("./_db.js");
+const { getTargettedUsers } = require("./_db.js");
 
 let messageFooter = "Manage notification settings at vaccinenotifications.org.";
 
 exports.handler = async (event, context, callback) => {
+    let data = JSON.parse(event.body);
 
-  let data = JSON.parse(event.body);
+    let selectedAgeGroups = data.selectedAgeGroups;
+    let province = data.province;
+    let postalCodes = data.postal;
+    let eligibilityGroups = data.eligibilityGroups;
 
-  let selectedAgeGroups = data.selectedAgeGroups;
-  let province = data.province;
-  let postalCodes = data.postal;
-  let eligibilityGroups = data.eligibilityGroups;
-  
-  let linkToBooking = data.linkToBooking;
-  let linkToSrc = data.linkToSrc;
-  let message = data.message;
-  let messageType = data.messageType;
-  let numberToBooking = data.numberToBooking;
+    let linkToBooking = data.linkToBooking;
+    let linkToSrc = data.linkToSrc;
+    let message = data.message;
+    let messageType = data.messageType;
+    let numberToBooking = data.numberToBooking;
 
-  let getUserBindings = async (
-    province,
-    postalCodes,
-    selectedAgeGroups,
-    eligibilityGroups
-  ) => {
+    let getUserBindings = async (
+        province,
+        postalCodes,
+        selectedAgeGroups,
+        eligibilityGroups
+    ) => {
+        let users = await getTargettedUsers(
+            province,
+            postalCodes,
+            selectedAgeGroups,
+            eligibilityGroups
+        );
 
-    let users = await getTargettedUsers(
-      province,
-      postalCodes,
-      selectedAgeGroups,
-      eligibilityGroups
+        let bindings = users.map((user) =>
+            JSON.stringify({
+                binding_type: "sms",
+                address: user.phoneNumber,
+                identity: user.id,
+            })
+        );
+
+        return bindings;
+    };
+
+    let getMessageBody = (
+        province,
+        postalCodes,
+        selectedAgeGroups,
+        eligibilityGroups
+    ) => {
+        let messageBody = messageType;
+
+        if (message) {
+            messageBody = messageBody + "\n\n" + message;
+        }
+
+        // start details we know
+        messageBody = messageBody + "\n\nDetails We Know: ";
+
+        if (province) {
+            let provinceText = province === "CA" ? "All Provinces" : province;
+            messageBody = messageBody + "\nProvince: " + provinceText;
+        }
+
+        if (postalCodes.length > 0) {
+            messageBody =
+                messageBody + "\nPostal Codes: " + postalCodes.join(", ");
+        }
+
+        if (selectedAgeGroups.length > 0) {
+            messageBody =
+                messageBody + "\nAge Groups: " + selectedAgeGroups.join(", ");
+        }
+
+        if (eligibilityGroups.length > 0) {
+            messageBody =
+                messageBody + "\nEligibility Groups: " + postalCodes.join(", ");
+        }
+
+        // end of details
+
+        if (linkToBooking) {
+            messageBody =
+                messageBody + "\n\nMore info + register here: " + linkToBooking;
+        }
+
+        if (numberToBooking) {
+            messageBody =
+                messageBody + "\n\nCall to book here: " + numberToBooking;
+        }
+
+        if (linkToSrc) {
+            messageBody = messageBody + "\n\nSource: " + linkToSrc;
+        }
+
+        messageBody = messageBody + "\n\n" + messageFooter;
+        return messageBody;
+    };
+
+    let sendMessages = async (userBindings, messageBody) => {
+        return await client.notify
+            .services(process.env.TWILIO_NOTIFY_SID)
+            .notifications.create({
+                toBinding: userBindings,
+                body: messageBody,
+            })
+            .then((notification) => {
+                console.log(notification.sid, "successfully sent");
+                return {
+                    statusCode: 200,
+                    body: `Message sent to ${userBindings.length}`,
+                };
+            })
+            .catch((error) => {
+                console.log(error);
+                return {
+                    statusCode: 500,
+                    body: `Error Sending Message: ${JSON.stringify(error)}`,
+                };
+            });
+    };
+
+    let userBindings = await getUserBindings(
+        province,
+        postalCodes,
+        selectedAgeGroups,
+        eligibilityGroups
     );
 
-    let bindings = users.map(user => JSON.stringify({
-      binding_type: 'sms',
-      address: user.phoneNumber,
-      identity: user.id,
-    }));
+    let messageBody = getMessageBody(
+        province,
+        postalCodes,
+        selectedAgeGroups,
+        eligibilityGroups
+    );
 
-    return bindings;
-  }
-
-  let getMessageBody = (province, postalCodes, selectedAgeGroups, eligibilityGroups) => {
-    let groups = _.concat([province], postalCodes, selectedAgeGroups, eligibilityGroups);
-    let messageBody = messageType;
-
-    if (message) {
-      messageBody = messageBody + "\n\n" + message;
-    }
-
-    // start details we know
-    messageBody = messageBody + "\n\nDetails We Know: ";
-
-    if (province) {
-      let provinceText = province === "CA" ? "All Provinces" : province;
-      messageBody = messageBody + "\nProvince: " + provinceText;
-    }
-
-    if (postalCodes.length > 0) {
-      messageBody = messageBody + "\nPostal Codes: " + postalCodes.join(", ");
-    }
-
-    if (selectedAgeGroups.length > 0) {
-      messageBody = messageBody + "\nAge Groups: " + selectedAgeGroups.join(", ");
-    }
-
-    if (eligibilityGroups.length > 0) {
-      messageBody = messageBody + "\nEligibility Groups: " + postalCodes.join(", ");
-    }
-
-    // end of details
-
-    if (linkToBooking) {
-      messageBody = messageBody + "\n\nMore info + register here: " + linkToBooking;
-    }
-
-    if (numberToBooking) {
-      messageBody = messageBody + "\n\nCall to book here: " + numberToBooking;
-    }
-
-    if (linkToSrc) {
-      messageBody = messageBody + "\n\nSource: " + linkToSrc;
-    }
-
-    messageBody = messageBody + "\n\n" + messageFooter;
-    return messageBody;
-  }
-
-  let sendMessages = async (userBindings, messageBody) => {
-    return await client.notify.services(process.env.TWILIO_NOTIFY_SID)
-    .notifications.create({
-      toBinding: userBindings,
-      body: messageBody
-    })
-    .then(notification => {
-      console.log(notification.sid, 'successfully sent');
-      return {
-        statusCode: 200,
-        body: `Message sent to ${userBindings.length}`,
-      };
-    })
-    .catch(error => {
-      console.log(error)
-      return {
-        statusCode: 500,
-        body: `Error Sending Message: ${JSON.stringify(error)}`
-      }
-    });
-  }
-
-  let userBindings = await getUserBindings(
-    province,
-    postalCodes,
-    selectedAgeGroups,
-    eligibilityGroups
-  );
-
-  let messageBody = getMessageBody(
-    province,
-    postalCodes,
-    selectedAgeGroups,
-    eligibilityGroups
-  );
-
-  return sendMessages(userBindings, messageBody);
-}
+    return sendMessages(userBindings, messageBody);
+};
