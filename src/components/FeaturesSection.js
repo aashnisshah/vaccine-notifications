8d778af7526124c7dc80b5c9eef479b0778b0d83
Joshua Wilkinson <joshjeffreywilkinson@gmail.com>
2021-05-20 14:36:19 -0400

    quick mobile button fix

--- src/components/FeaturesSection.js
+++ src/components/FeaturesSection.js
@@ -59,7 +59,7 @@ function FeaturesSection(props) {
                     spaced={true}
                     className="text-center"
                 />
-                {props.isMobile && <DownloadButtons />}
+                {!props.isMobile && <DownloadButtons />}
                 <div className="FeaturesSection__features">
                     {items.map((item, index) => (
                         <Row className="align-items-center" key={index}>
